06e06a6d061e4c3db743d00b421e25e167d4d2ee
Jon Sanders <Jon.Sanders@redbox.com>
2020-01-15 13:21:16 -0600

    add um stuff

--- src/Components/Simulation.js
+++ src/Components/Simulation.js
@@ -1,19 +1,29 @@
 import React from "react";
+import Ticker from "./Ticker";
 
 export default function Simulation(props) {
-
-    console.log(props.character);
-
     let content;
     if (props.character.name != null) {
         content = <p>{props.character.name} enters the Lair. <br/>
             {props.character.name} walks up to a Gru. <br/>
             {props.character.name} was eaten by a Gru. </p>;
-    }
-    else
+    } else {
         content = <p>Press Start!</p>
+    }
+
+    const adventurer = createAdventurer(props.character);
+    console.log(adventurer);
     return (
         <div className="Simulation">
             {content}
+            <Ticker adventurer={adventurer}
+                    intervalMs='10000'/>
         </div>);
+}
+
+function createAdventurer(character) {
+    console.log("CREATING");
+    console.log(character);
+    let adventurer = {...character};
+    return (adventurer);
 }

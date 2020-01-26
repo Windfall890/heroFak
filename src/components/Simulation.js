658a30f451995ee3e2797e33700c99eba76449f6
Eric <eric.burger@redbox.com>
2020-01-26 11:39:32 -0600

    no this.context

--- src/components/Simulation.js
+++ src/components/Simulation.js
@@ -3,14 +3,14 @@ import Ticker from "./Ticker";
 import { GameContext } from "../context/GameContext"
 
 function Simulation() {
-    const context = useContext(GameContext)
-    let content = context.character.name.isEmpty ?
-        "" : <div>{context.character.name} has started a quest.</div>
+    const { character } = useContext(GameContext)
+    let content = character.name.isEmpty ?
+        "" : <div>{character.name} has started a quest.</div>
 
     return (
         <div className="simulation">
             {content}
-            <Ticker adventurer={context.character}
+            <Ticker adventurer={character}
                     intervalMs='10000'/>
         </div>
     )
